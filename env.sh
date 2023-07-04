34a47685e05c2a8946a020246e065dffb4566c9f
aryans1204 <arshar1204@gmail.com>
2023-07-04 20:03:52 +0800

    Env file and template YAML config

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PROJECT_ID='fulcrum-391806'
+export LOCATION='asia-southeast1'
+export IMAGE_URL='some Artifact Regsitry URL'
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
-'''
-    Package initialization
-'''
+from src.fulcrum.models.chatbot import Chatbot
+from src.fulcrum.models.user import User, LoginUser, SignUpUser
+
+ __all__ = [
+    "Chatbot",
+     "User",
+     "LoginUser",
+     "SignUpUser"
+ ]
