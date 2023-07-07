06bf7e6034cc2914be3caa93659a0d6fe694507e
aryans1204 <arshar1204@gmail.com>
2023-07-07 12:40:18 +0800

    ChromaDB CRUD implementation

---
+++ src/gcloud/vectordb.py
@@ -0,0 +1,47 @@
+import os
+import chromadb
+from chromadb.config import Settings
+
+def insertDB(file_path: str, username: str, chatbot_id: str):
+    '''
+        Utility to insert a PDF into the ChromaDB database as Sentence transformer vectors
+    '''
+    chunks = parsePDF(file_path)
+    client = chromadb.Client(
+        Settings(
+            chroma_api_impl="rest",
+            chroma_server_host=os.environ["CHORMA_URL"],
+            chroma_server_http_port=os.environ["CHROMA_PORT"]
+        )
+    )
+
+    collection = client.get_or_create_collection(name=username+chatbot_id)
+    ids = []
+    texts = []
+    metadatas = []
+    cur_len = collection.count()
+
+    for c in chunks:
+        cur_len += 1
+        ids.append("id"+str(cur_len))
+        texts.append(c)
+        metadatas.append({"id": c})
+
+    collection.add(
+        documents=texts,
+        metadatas=metadatas,
+        ids=ids
+    )
+
+def deleteDB(username: str, chatbot_id: str):
+    client = chromadb.Client(
+        Settings(
+            chroma_api_impl="rest",
+            chroma_server_host=os.environ["CHROMA_URL"],
+            chroma_server_http_port=os.environ["CHROMA_PORT"]
+        )
+    )
+
+    collection = client.delete_collection(name=username+chatbot_id)
+
+
