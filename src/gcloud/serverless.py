e9dcd8d67756ad29eabba5ac5dc0d49b2842e567
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2023-07-23 01:55:41 +0800

    Merge branch 'master' of https://github.com/aryans1204/Fulcrum.ai

--- src/gcloud/serverless.py
+++ src/gcloud/serverless.py
@@ -1,16 +1,15 @@
-from fulcrum.models.chatbot import Chatbot
 import os
 import subprocess
 import yaml
 import tempfile
 import re
 
-def deployChatbot(chatbot: Chatbot, username: str) -> str:
+def deployChatbot(chatbot: dict, username: str) -> str:
     '''
         Utility to deploy a given chatbot
         returns a URL string of the deployed resource.
     '''
-    with open("services.yaml", "r") as stream:
+    with open(f"{os.environ['YAML_DIR']}/services.yaml", "r") as stream:
         try:
             d = yaml.safe_load(stream)
         except yaml.YAMLError as e:
@@ -25,15 +24,16 @@ def deployChatbot(chatbot: Chatbot, username: str) -> str:
     d["spec"]["template"]["spec"]["containers"][0]["env"][6]["value"] = os.environ["CHROMA_URL"]
     d["spec"]["template"]["spec"]["containers"][0]["env"][7]["value"] = os.environ["CHROMA_PORT"]
     d["metadata"]["name"] = username+chatbot["chatbot_id"]
-    file = open("services_temp.yaml", "w")
+    file = open(f"{os.environ['YAML_DIR']}/services_temp.yaml", "w")
     yaml.dump(d, file)
     file.close()
     subprocess.run(["gcloud", "run", "services", 
-                                 "replace", f"{os.environ['PWD']}/services_temp.yaml"])
+                                 "replace", f"{os.environ['YAML_DIR']}/services_temp.yaml"])
     
     service = str(d["metadata"]["name"])
     output = subprocess.run(["gcloud", "run", "services", "describe", f"{service}"], capture_output=True).stdout
     output = re.findall("https.*app", str(output))
+    subprocess.run(["rm", f"{os.environ['YAML_DIR']}/services_temp.yaml"])
     return output[0]
 
 def deleteChatbot(service_name: str):
