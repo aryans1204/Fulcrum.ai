851ea4462b07ed0a1375282e4dadb2b2de2b979c
aryans1204 <arshar1204@gmail.com>
2023-07-22 14:09:46 +0800

    Bucket storagr fixed and tests passing

--- src/gcloud/bucket_storage.py
+++ src/gcloud/bucket_storage.py
@@ -11,20 +11,28 @@ def createBucket(bucket_name: str):
     new_bucket = storage_client.create_bucket(bucket, location=os.environ["LOCATION"])
     return new_bucket.name
 
+def deleteObj(bucket_name: str, blob_name: str):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(bucket_name)
+    blob.reload()
+    generation_match_precondition = blob.generation
+    blob.delete(if_generation_match=generation_match_precondition)
+
+
 def deleteBucket(bucket_name: str):
     '''
         Utility for deleting a bucket in GCP Cloud Storage
     '''
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    bucket.delete()
+    bucket = storage_client.bucket(bucket_name)
+    bucket.delete(force=True)
 
 def uploadObj(bucket_name, file_path, fileName):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(fileName)
-
+    generation_match_precondition = 0
     blob.upload_from_filename(file_path, if_generation_match=generation_match_precondition)
-
 
 
